078335866fc8651dcfaddeb86f78d8b28334b5ad
zorba11 <zorbathegreek111@gmail.com>
2020-05-08 11:55:55 -0400

    google sigin, custom-button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -37,7 +39,10 @@ class SignIn extends React.Component{
                     <FormInput name='email' type='email' value={this.state.email} label='email' handleChange={this.handleChange} required />
                     <FormInput name='password' type='password' value={this.state.password} label='password' handleChange={this.handleChange} required />
 
+                    <div className='buttons'>
                     <CustomButton type='submit' value='submit form'>Sign in</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn >Sign in with Google</CustomButton>
+                    </div>
                  </form>
             </div>
         )
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyAnadeCte5fBKbjlomAvdkQMz4uqZ0Nqqg",
+    authDomain: "portfolio-2020-d74be.firebaseapp.com",
+    databaseURL: "https://portfolio-2020-d74be.firebaseio.com",
+    projectId: "portfolio-2020-d74be",
+    storageBucket: "portfolio-2020-d74be.appspot.com",
+    messagingSenderId: "884437872497",
+    appId: "1:884437872497:web:6c324c7ae4cd0bb94950d0"
+  };
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
+
